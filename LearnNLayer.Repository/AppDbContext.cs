0e2e22d68b5a7237ce90955ba1c5afca6580c20d
Enes Tunahan <enesstunahan@gmail.com>
2022-05-21 17:57:05 +0300

    AppDbContext

---
+++ LearnNLayer.Repository/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LearnNLayer.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearnNLayer.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+    }
+}
