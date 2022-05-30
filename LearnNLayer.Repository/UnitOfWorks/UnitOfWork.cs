4bbde982eecb1b82df78445c00475d9c3190a628
Enes Tunahan <enesstunahan@gmail.com>
2022-05-30 22:32:51 +0300

    Unit of work

---
+++ LearnNLayer.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using LearnNLayer.Core.IUnitOfWork;
+using System.Threading.Tasks;
+
+namespace LearnNLayer.Repository.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+          await _context.SaveChangesAsync();
+        }
+    }
+}
